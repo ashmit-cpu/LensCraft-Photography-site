e59a8237a04821852f3c2c299f3a6bdf120febcc
Ashmit k <ashmithkacheri138@gmail.com>
2023-07-14 17:53:40 +0530

    changed banner image

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -5,8 +5,8 @@ function Banner() {
   return (
     <div>
         <div className='banner_img'>
-          <img className="bg-img"src="assets\img\wallpaperflare.com_wallpaper.jpg" alt="" />
-          <img className="bg-mob-img"src="assets\img\wallpaperflare.com_wallpaper-mob.jpg" alt="" />
+          <video autoPlay muted loop playsInline className="bg-img"src="https://player.vimeo.com/external/387169338.sd.mp4?s=59861f43b372ae5fcfb9160c2c53ae7460a1ffcf&profile_id=164&oauth2_token_id=57447761" alt="" />
+          <video autoPlay muted loop playsInline className="bg-mob-img"src="https://player.vimeo.com/external/387169338.sd.mp4?s=59861f43b372ae5fcfb9160c2c53ae7460a1ffcf&profile_id=164&oauth2_token_id=57447761" alt="" />
 
           <img className="logo"src="assets\img\Lens_craft_logo.png" alt="" />
         </div>
